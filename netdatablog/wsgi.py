534d6df2090ddc18060333b47ccb1fc6f5075110
huhuaiyong <692673390@qq.com>
2015-06-04 22:06:23 +0800

    init

--- netdatablog/wsgi.py
+++ netdatablog/wsgi.py
@@ -6,8 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
 import os
+import sys
+path = '/netdatablog/netdatablog'
+if path not in sys.path:
+    sys.path.append(path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netdatablog.settings")
 
 from django.core.wsgi import get_wsgi_application
